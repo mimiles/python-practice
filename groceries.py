32599fc49d4222a3a9ba7d3b2c0ec21088bcb78e
Mike Miles <mm8932@stern.nyu.edu>
2017-07-10 20:32:24 -0400

    Print in alphabetical order the name of each product, and include its price rounded to two decimal places

--- groceries.py
+++ groceries.py
@@ -30,6 +30,7 @@ print("THERE ARE " + str(len(products)) + " PRODUCTS:")
 products = sorted(products, key=operator.itemgetter("name"))
 
 for product in products:
-    print(" + " + product["name"])
-    
+    price_usd = ' (${0:.2f})'.format(product["price"])
+    print(" + " + product["name"] + price_usd)
+
 # code.interact(local=locals())
